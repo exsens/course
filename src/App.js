7526f1fa93506ba023957c56dac21d39cd5cf3aa
exsens <knyazex@mail.ru>
2022-05-23 17:46:05 +0300

    lesson 7, components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-function App() {
+import Header from './layout/header';
+import Content from './layout/content';
+import Footer from './layout/footer';
+
+const App = () => {
   return (
     <div className="App">
-     
+      <Header />
+      <Content />
+      <Footer />
     </div>
   );
 }
---
+++ src/layout/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Logo from '../components/logo/logo';
+import Nav from '../components/nav/nav';
+
+const Header = () => {
+  return (
+    <header>
+      <Logo />
+      <Nav />
+    </header>
+  )
+}
+
+export default Header;
