c09c26365b12ba1b9159ddd41906a5a1935b5ec9
unknown <exgedex@gmail.com>
2022-05-28 00:34:40 +0300

    les 54, component lifecycle

--- src/components/users-list/users-list.jsx
+++ src/components/users-list/users-list.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import axios from "axios";
 import * as api from "../../api/config";
@@ -7,38 +8,23 @@ import { setUsers, toggleFollow } from "../../store/users/users-action.js";
 import UsersItem from "../users-item/users-item.jsx";
 
 class UsersList extends Component {
-  constructor(props) {
-    super(props)
+  componentDidMount() {
+    const { setUsers } = this.props;
 
     axios
       .get(api.getUsers())
-      .then((data) => this.props.setUsers(data.data))
+      .then((data) => setUsers(data.data))
       .catch((error) => console.log(error));
+    console.log("load", this.props);
   }
-
 
   render() {
-    return this.users ? (
-      this.props.users.map((user) => (
-        <UsersItem key={user.id} toggleFollow={this.props.toggleFollow} {...user} />
-      ))
-    ) : (
-      <h3>Loading...</h3>
-    );
+    const { users, toggleFollow } = this.props;
+    return users.map((user) => (
+      <UsersItem key={user.id} toggleFollow={toggleFollow} {...user} />
+    ));
   }
 }
-
-// const UsersList = ({ users, toggleFollow, setUsers }) => {
-
-//   if (!users.length) {
-//     axios.get(api.getUsers())
-//       .then((data) => setUsers(data.data))1
-//       .catch((error) => console.log(error))
-//   }
-//   return (
-//
-//   )
-// }
 
 const mapStateToProps = (state) => {
   return {
@@ -46,16 +32,9 @@ const mapStateToProps = (state) => {
   };
 };
 
-const mapStateToDispatch = (dispatch) => {
-  return {
-    setUsers: (data) => {
-      dispatch(setUsers(data));
-    },
-
-    toggleFollow: (id) => {
-      dispatch(toggleFollow(id));
-    },
-  };
+const mapStateToDispatch = {
+  setUsers,
+  toggleFollow,
 };
 
 export default connect(mapStateToProps, mapStateToDispatch)(UsersList);
