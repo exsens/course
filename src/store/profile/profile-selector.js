220284f10898e192bb142017f3221aa1aee2a814
exsens <knyzex@mail.ru>
2022-06-02 12:26:42 +0300

    les 83, reselect

--- src/store/profile/profile-selector.js
+++ src/store/profile/profile-selector.js
@@ -1,3 +1,9 @@
+// import { createSelector } from 'reselect';
+
 export const selectProfileData = (state) => state.profileData;
 export const selectProfileInfo = (state) => state.profileData.profileInfo;
 export const selectProfilePosts = (state) => state.profileData.posts;
+
+// const getCasualPosts = (selectProfilePosts, (posts) => {
+//   return posts.filter((post) => true)
+// }) 
